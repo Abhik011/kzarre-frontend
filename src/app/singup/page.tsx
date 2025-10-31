ecdb0046b568ff871b07b2a7cc323ea3a77e49ab
Abhijeet kulkarni <kulkarniabhi011@gmail.com>
2025-10-31 22:25:05 +0530

    login and singup contenct added

---
+++ src/app/singup/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from "react";
+
+export default function SingupPage() {
+  return (
+    <div>
+      <h1>Singup</h1>
+      <p>Please enter your credentials.</p>
+    </div>
+  );
+}
