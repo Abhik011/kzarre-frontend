7b87ad70ece778eaf489317bbb601bb5cccc25d0
Abhijeet kulkarni <ak@Abhijeets-MacBook-Air.local>
2025-12-11 06:15:40 +0530

    adsf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,18 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { Analytics } from "@vercel/analytics/react";
+
 
 
 export default function HomePage() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push("/login");
+    router.push("/home");
   }, [router]);
 
   return (
     <>
-      <Analytics />
     </>
   );
 }
