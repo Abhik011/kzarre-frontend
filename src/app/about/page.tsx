e5d4cf868bf8a939f1368e4d1cd4098af3ad8fdf
Abhijeet kulkarni <ak@Abhijeets-MacBook-Air.local>
2025-12-13 18:59:22 +0530

    ad

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -22,7 +22,7 @@ export default function AboutPage() {
         <p className="quote">
           Inspired by the sacred wisdom of the Bhagavad Gita,
           <span> “Karmanye vadhikaraste ma phaleshu kadachana” </span> — our
-          philosophy embraces purposeful creation. depolyments working
+          philosophy embraces purposeful creation. switching checking to push 
         </p>
 
         <p className="intro">
